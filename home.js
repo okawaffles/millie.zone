43700ad00fbc057ee4016fcf37064f3224ae3055
Millie Aurora <oka@waffle.moe>
2025-07-08 23:11:21 -0400

    first commit

---
+++ home.js
@@ -0,0 +1,21 @@
+const DEBUG = false;
+
+$(document).ready(() => {
+    setTimeout(() => {
+        $('#init').css('display', 'none');
+        $('#after').css('display', 'inline');
+
+        setTimeout(() => {
+            $('#content-container').css('display', 'flex');
+        }, 1000);
+    }, DEBUG?0:2900);
+
+    $('#gll').on('click', () => {
+        $('#splash').text('Good Luck!');
+        $('#init').css('display', 'flex');
+        $('#after').css('display', 'none');
+        setTimeout(() => {
+            window.location = '/goodluckle';
+        }, 1500);
+    });
+});
